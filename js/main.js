a18c968584d56b5697da5c0a2b05ab483b2ac33a
Ke Qian <kq32@cornell.edu>
2015-10-15 00:03:47 -0400

    prototype at the end of hackthon

---
+++ js/main.js
@@ -0,0 +1,21 @@
+/* smooth it up */
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+$(document).ready(function(){
+  $(".spec_tbl tr").children('td').addClass('light');
+  $(".spec_tbl tr").children('td:first-child').removeClass('light');
+});
+
